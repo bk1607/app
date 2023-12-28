21662ab1c4abbbfbad8c8cd519aca9342f72b55a
bk1607 <baluanumala16@gmail.com>
2023-12-28 11:16:11 +0530

    instance_profile

---
+++ main.tf
@@ -0,0 +1,32 @@
+# launch template for auto scaling group
+resource "aws_launch_template" "main" {
+  name = "main"
+
+
+
+
+  image_id = data.aws_ami.ami_id.id
+
+
+
+  instance_type = "t2.nano"
+
+
+
+
+  placement {
+    availability_zone = "us-west-2a"
+  }
+
+  
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "test"
+    }
+  }
+
+  user_data = filebase64("${path.module}/example.sh")
+}
