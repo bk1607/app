c4fc9f59db303cbe44faa7b34dc8ab4d7c9d669f
bk1607 <baluanumala16@gmail.com>
2024-01-06 01:17:25 +0530

    error in user data

--- example.sh
+++ example.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-pull -i localhost, -U https://github.com/bk1607/learn-ansible roboshop/roboshop.yml -e role_name=${var.name} -e env=${var.env} > /var/log/ansible.log
+ansible-pull -i localhost, -U https://github.com/bk1607/learn-ansible roboshop/roboshop.yml -e role_name=${name} -e env=${env} > /var/log/ansible.log
