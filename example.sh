791c5262326b4c279db8f7e3ad970b5087af2dbe
bk1607 <baluanumala16@gmail.com>
2024-01-07 12:51:39 +0530

    configuring app module

--- example.sh
+++ example.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo labauto ansible
 ansible-pull -i localhost, -U https://github.com/bk1607/learn-ansible roboshop/roboshop.yml -e role_name=${name} -e env=${env} > /var/log/ansible.log
