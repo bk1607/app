6e0042f80f95222a0817af8c032cd75d270a0fc2
bk1607 <baluanumala16@gmail.com>
2024-01-13 19:54:34 +0530

    debugging

--- main.tf
+++ main.tf
@@ -103,3 +103,20 @@ resource "aws_route53_record" "main" {
   ttl     = 300
   records = [var.lb_dns_name]
 }
+
+# to create rules in listener
+#resource "aws_lb_listener_rule" "main" {
+#  listener_arn = var.listener_arn
+#  priority     = var.priority
+#
+#  action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.main.arn
+#  }
+#
+#  condition {
+#    host_header {
+#      values = [local.dns_name]
+#    }
+#  }
+#}
