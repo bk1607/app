8c4ec957e4e7752eeb70f3309290de6f020ed1bd
bk1607 <baluanumala16@gmail.com>
2024-01-13 20:13:46 +0530

    debugging

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  dns_name = var.name == "frontend" ? "${var.env}" : "${var.name}-${var.env}"
+  dns_name = var.name == "frontend" ? "${var.env}.devops2023.online" : "${var.name}-${var.env}.devops2023.online"
   dns_word = var.env  == "prod"  ? "www" : var.env
 }
